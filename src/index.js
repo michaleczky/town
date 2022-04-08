5a29060583a4ced4c6a3df4c7ac5f0e623b87014
michaleczky <michaleczky@gmail.com>
2022-04-08 22:08:55 +0200

    első lépések

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import "./styles.css";
 
 document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
+
 `;
+
+var rajz = document.getElementById("rajz");
+var ct = rajz.getContext("2d");
+ct.beginPath();
+ct.rect(100, 200, 200, 200);
+ct.moveTo(100, 200);
+ct.lineTo(200, 60);
+ct.lineTo(300, 200);
+ct.stroke();
